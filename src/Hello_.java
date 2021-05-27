0f83b9a9842c7e8d8d302d7cf413a1dfc9ecef1c
Prasad <Prasad@DESKTOP-B7985R6>
2021-05-27 16:48:53 +0530

    First commit

---
+++ src/Hello_.java
@@ -0,0 +1,21 @@
+
+
+public class Hello_{
+     int a=5;
+	public void getme() {
+
+		System.out.println("i am inside method getdata");
+
+	}
+
+	public static void main(String[] args) {
+
+		Hello_ a = new Hello_();
+		a.getme();
+		System.out.println(a.a);
+		System.out.println("Hi its me prasad");
+		System.out.println("Don't ruin my java please");
+
+	}
+}
+
